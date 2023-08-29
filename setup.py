a210d7ac6128f232b5c37465ccebbda996ca4194
Anas Bouzid <anasbouzid@gmail.com>
2023-08-29 11:35:02 -0500

    update package metadata

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import setuptools
 
 setuptools.setup(
     name="streamlit-code-editor",
-    version="0.1.8",
+    version="0.1.9",
     author="Anas Bouzid",
     author_email="anasbouzid@gmail.com",
     description="React-ace editor customized for Streamlit",
     long_description="React-ace component with custom themes wrapped with customizable interface elements for better integration as a Streamlit code editor",
     long_description_content_type="text/plain",
-    url="https://github.com/bouzidanas/streamlit.io/tree/dev/streamlit-code-editor",
+    url="https://github.com/bouzidanas/streamlit-code-editor",
     packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=[],
