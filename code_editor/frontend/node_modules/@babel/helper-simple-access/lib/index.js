952923f5ecc334ed89f85a3b518800107f85dbd4
bouzidanas <anasbouzid@gmail.com>
2024-05-30 06:30:03 -0500

    update frontend build

--- code_editor/frontend/node_modules/@babel/helper-simple-access/lib/index.js
+++ code_editor/frontend/node_modules/@babel/helper-simple-access/lib/index.js
@@ -17,42 +17,6 @@ const {
   unaryExpression
 } = _t;
 const simpleAssignmentVisitor = {
-  UpdateExpression: {
-    exit(path) {
-      const {
-        scope,
-        bindingNames,
-        includeUpdateExpression
-      } = this;
-      if (!includeUpdateExpression) {
-        return;
-      }
-      const arg = path.get("argument");
-      if (!arg.isIdentifier()) return;
-      const localName = arg.node.name;
-      if (!bindingNames.has(localName)) return;
-
-      if (scope.getBinding(localName) !== path.scope.getBinding(localName)) {
-        return;
-      }
-      if (path.parentPath.isExpressionStatement() && !path.isCompletionRecord()) {
-        const operator = path.node.operator == "++" ? "+=" : "-=";
-        path.replaceWith(assignmentExpression(operator, arg.node, numericLiteral(1)));
-      } else if (path.node.prefix) {
-        path.replaceWith(assignmentExpression("=", identifier(localName), binaryExpression(path.node.operator[0], unaryExpression("+", arg.node), numericLiteral(1))));
-      } else {
-        const old = path.scope.generateUidIdentifierBasedOnNode(arg.node, "old");
-        const varName = old.name;
-        path.scope.push({
-          id: old
-        });
-        const binary = binaryExpression(path.node.operator[0], identifier(varName),
-        numericLiteral(1));
-
-        path.replaceWith(sequenceExpression([assignmentExpression("=", identifier(varName), unaryExpression("+", arg.node)), assignmentExpression("=", cloneNode(arg.node), binary), identifier(varName)]));
-      }
-    }
-  },
   AssignmentExpression: {
     exit(path) {
       const {
@@ -65,33 +29,63 @@ const simpleAssignmentVisitor = {
       seen.add(path.node);
       const left = path.get("left");
       if (!left.isIdentifier()) return;
-
       const localName = left.node.name;
       if (!bindingNames.has(localName)) return;
-
       if (scope.getBinding(localName) !== path.scope.getBinding(localName)) {
         return;
       }
       const operator = path.node.operator.slice(0, -1);
       if (LOGICAL_OPERATORS.includes(operator)) {
-        path.replaceWith(logicalExpression(
-        operator, path.node.left, assignmentExpression("=", cloneNode(path.node.left), path.node.right)));
+        path.replaceWith(logicalExpression(operator, path.node.left, assignmentExpression("=", cloneNode(path.node.left), path.node.right)));
       } else {
-        path.node.right = binaryExpression(
-        operator, cloneNode(path.node.left), path.node.right);
+        path.node.right = binaryExpression(operator, cloneNode(path.node.left), path.node.right);
         path.node.operator = "=";
       }
     }
   }
 };
-function simplifyAccess(path, bindingNames,
-includeUpdateExpression = true) {
-  path.traverse(simpleAssignmentVisitor, {
-    scope: path.scope,
-    bindingNames,
-    seen: new WeakSet(),
-    includeUpdateExpression
-  });
+{
+  simpleAssignmentVisitor.UpdateExpression = {
+    exit(path) {
+      if (!this.includeUpdateExpression) return;
+      const {
+        scope,
+        bindingNames
+      } = this;
+      const arg = path.get("argument");
+      if (!arg.isIdentifier()) return;
+      const localName = arg.node.name;
+      if (!bindingNames.has(localName)) return;
+      if (scope.getBinding(localName) !== path.scope.getBinding(localName)) {
+        return;
+      }
+      if (path.parentPath.isExpressionStatement() && !path.isCompletionRecord()) {
+        const operator = path.node.operator === "++" ? "+=" : "-=";
+        path.replaceWith(assignmentExpression(operator, arg.node, numericLiteral(1)));
+      } else if (path.node.prefix) {
+        path.replaceWith(assignmentExpression("=", identifier(localName), binaryExpression(path.node.operator[0], unaryExpression("+", arg.node), numericLiteral(1))));
+      } else {
+        const old = path.scope.generateUidIdentifierBasedOnNode(arg.node, "old");
+        const varName = old.name;
+        path.scope.push({
+          id: old
+        });
+        const binary = binaryExpression(path.node.operator[0], identifier(varName), numericLiteral(1));
+        path.replaceWith(sequenceExpression([assignmentExpression("=", identifier(varName), unaryExpression("+", arg.node)), assignmentExpression("=", cloneNode(arg.node), binary), identifier(varName)]));
+      }
+    }
+  };
+}
+function simplifyAccess(path, bindingNames) {
+  {
+    var _arguments$;
+    path.traverse(simpleAssignmentVisitor, {
+      scope: path.scope,
+      bindingNames,
+      seen: new WeakSet(),
+      includeUpdateExpression: (_arguments$ = arguments[2]) != null ? _arguments$ : true
+    });
+  }
 }
 
 //# sourceMappingURL=index.js.map
